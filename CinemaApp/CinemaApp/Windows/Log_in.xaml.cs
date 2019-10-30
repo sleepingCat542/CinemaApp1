03b387da14408f8a9fcd56184bdef0f518f5470b
Yuliana <yuki.sai@mail.ru>
2019-10-30 22:13:45 +0300

    initial application design

---
+++ CinemaApp/CinemaApp/Windows/Log_in.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CinemaApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Log_in.xaml
+    /// </summary>
+    public partial class Log_in : Window
+    {
+        public Log_in()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegestration_Click(object sender, RoutedEventArgs e)
+        {
+            Regestration registraition = new Regestration();
+            registraition.Show();
+            Close();
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ CinemaApp/CinemaApp/Windows/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnWide_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState== WindowState.Normal)
+            WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnThink_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Main_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Base_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
